4b2c17955f4662ca47821064f186571376011f2f
emadkhazali-dev <your_emadkhazali@gmail.com>
2026-03-28 05:33:03 +0300

    add root route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send("Server is working 🚀");
+app.get('/webhook', (req, res) => {
+  res.send('Webhook is working 🚀');
 });
 app.use(express.json());
 
