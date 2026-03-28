c8b5a82abb7a6c4e350c0cf059a1e9db348a8624
emadkhazali-dev <your_emadkhazali@gmail.com>
2026-03-28 04:59:23 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = "test123";
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("AL_QAED SERVER WORKING");
+});
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  return res.status(403).send("Verification failed");
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("📩 NEW MESSAGE:");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
